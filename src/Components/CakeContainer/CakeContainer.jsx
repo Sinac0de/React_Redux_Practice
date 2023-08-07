74c5a4b2e88da4cc339c1e7b3d4c12a20bc014ae
Sina Moradian <sinacsharp@gmail.com>
2023-08-07 14:17:53 +0330

    Create Cake purchase component

---
+++ src/Components/CakeContainer/CakeContainer.jsx
@@ -0,0 +1,10 @@
+const CakeContainer = () => {
+  return (
+    <div>
+      <h2>num of cakes - </h2>
+      <button>Buy Cake</button>
+    </div>
+  );
+};
+
+export default CakeContain < div > er;
