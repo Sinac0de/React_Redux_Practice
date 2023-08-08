1880c458df1a504716d05cbe94465d564f4be41f
Sina Moradian <sinacsharp@gmail.com>
2023-08-08 10:49:16 +0330

    Add useSelector & useDispatch

--- src/Components/CakeContainer/CakeContainer.jsx
+++ src/Components/CakeContainer/CakeContainer.jsx
@@ -1,10 +1,15 @@
+import { useSelector, useDispatch } from "react-redux";
+import { buyCake } from "../../redux/cake/cakeActions";
+
 const CakeContainer = () => {
+  const numOfCakes = useSelector((state) => state.numOfCakes);
+  const dispatch = useDispatch();
   return (
     <div>
-      <h2>num of cakes - </h2>
-      <button>Buy Cake</button>
+      <h2>num of cakes - {numOfCakes}</h2>
+      <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
     </div>
   );
 };
 
-export default CakeContain < div > er;
+export default CakeContainer;
