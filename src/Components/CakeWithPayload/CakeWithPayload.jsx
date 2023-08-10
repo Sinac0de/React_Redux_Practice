48928cde4c7608837b1678b02ab1c90f604d52ec
Sina Moradian <sinacsharp@gmail.com>
2023-08-10 19:59:27 +0330

    Create cake with payload component

---
+++ src/Components/CakeWithPayload/CakeWithPayload.jsx
@@ -0,0 +1,20 @@
+import { useSelector, useDispatch } from "react-redux";
+import { buyCake } from "../../redux/cake/cakeActions";
+import { useState } from "react";
+
+const CakeWithPayload = () => {
+  const [numbers, setNumbers] = useState(0);
+  const numOfCakes = useSelector((state) => state.cake.numOfCakes);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>num of cakes - {numOfCakes}</h2>
+      <input type="text" onChange={(e) => setNumbers(e.target.value)} />
+      <button onClick={() => dispatch(buyCake(numbers))}>
+        Buy {numbers} Cake
+      </button>
+    </div>
+  );
+};
+
+export default CakeWithPayload;
