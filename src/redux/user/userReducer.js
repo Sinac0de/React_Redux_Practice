d8308e7e4b69c28b1bfba11009ab054d52075cbc
Sina Moradian <sinacsharp@gmail.com>
2023-08-10 23:50:45 +0330

    Add userReducer

---
+++ src/redux/user/userReducer.js
@@ -0,0 +1,26 @@
+import {
+  FETCH_USERS_FAILURE,
+  FETCH_USERS_REQUEST,
+  FETCH_USERS_SUCCESS,
+} from "./userTypes";
+
+const initialState = {
+  loading: false,
+  data: [],
+  error: "",
+};
+
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return { ...state, loading: true };
+    case FETCH_USERS_SUCCESS:
+      return { loading: false, error: "", data: action.payload };
+    case FETCH_USERS_FAILURE:
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
